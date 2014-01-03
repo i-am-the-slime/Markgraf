1018ea90c16beb667de7e2c1ea7c630e16453cb2
Mark Eibes <eibes@me.com>
2014-01-03 23:14:08 +0100

    Added possibility to test.
    Tests are in src/instrumentTest. There are examples for java and scala.

---
+++ src/instrumentTest/scala/de/markeibes/HorizontalBarChartTest2.scala
@@ -0,0 +1,10 @@
+package de.markeibes
+
+import android.test.ActivityInstrumentationTestCase2
+import de.markeibes.HorizontalBarChart
+
+/**
+ * Created by mark on 03.01.14.
+ */
+class HorizontalBarChartTest2() extends ActivityInstrumentationTestCase2[HorizontalBarChart](classOf[HorizontalBarChart]) {
+}
