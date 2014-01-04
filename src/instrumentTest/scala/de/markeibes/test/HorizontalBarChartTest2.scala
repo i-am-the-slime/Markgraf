8a1fe630046d70c7765e2c2895a11b21110b0ff0
Mark Eibes <eibes@me.com>
2014-01-04 10:26:18 +0100

    Basic setup complete.

--- src/instrumentTest/scala/de/markeibes/test/HorizontalBarChartTest2.scala
+++ src/instrumentTest/scala/de/markeibes/test/HorizontalBarChartTest2.scala
@@ -1,14 +1,14 @@
-package de.markeibes
+package de.markeibes.test
 
 import android.test.ActivityInstrumentationTestCase2
 import android.app.Activity
 import android.widget.TextView
 import junit.framework.Assert._
+import de.markeibes.HorizontalBarChart
 
 class HorizontalBarChartTest2 extends ActivityInstrumentationTestCase2[HorizontalBarChart](classOf[HorizontalBarChart]) {
   lazy val activity = getActivity
   lazy val tv = activity.findViewById(de.markeibes.R.id.tv).asInstanceOf[TextView]
-
 
   override def setUp(): Unit = {
     super.setUp()
